4daa18fe7c06195d1696acc8984bd2cd5174fc01
buhuan <buhuan@talkweb.com.cn>
2019-08-08 14:37:20 +0800

    增加readme说明 增加仓库URL格式校验 完善异常代码处理

---
+++ README.md.md
@@ -0,0 +1,35 @@
+#Dockcmd-maven-plugin
+
+## 插件状态: Alpha
+
+
+###怎么安装插件？
+在IDEA中 使用 `maven install` 命令即可把maven插件安装到本地仓库中
+> 
+###怎么使用插件？
+在开发项目中 **pom.xml** 文件中引用
+
+```xml
+<plugin>
+         <groupId>com.talkweb.plugin</groupId>
+          <artifactId>dockercmd-maven-plugin</artifactId>
+          <version>1.1.2</version>
+          <configuration>
+              <container_ports>8080</container_ports>
+              <host_ports>8080</host_ports>
+              <remote_HTTPSAPI>https://api-docker.talkyun.com.cn:2376</remote_HTTPSAPI>
+              <completetag>${docker.image.prefix}/registry/${project.build.finalName}${project.version}}</completetag>
+          </configuration>
+</plugin>
+```
+配置解释
+
+`<container_ports>` 你的容器端口
+
+`<host_ports>` 你的主机端口
+
+相当于把docker容器的端口映射到主机端口，使容器能内外网访问
+
+`<remote_HTTPSAPI>` docker服务远程API接口地址（目前仅支持https加密访问）
+
+`<completetag>` 镜像的完整标签（TAG）
--- src/main/java/com/talkweb/dockerplugin/RunMojo.java
+++ src/main/java/com/talkweb/dockerplugin/RunMojo.java
@@ -1,20 +1,27 @@
 package com.talkweb.dockerplugin;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.spotify.docker.client.DefaultDockerClient;
+import com.spotify.docker.client.DockerCertificates;
 import com.spotify.docker.client.DockerClient;
 import com.spotify.docker.client.exceptions.DockerCertificateException;
 import com.spotify.docker.client.exceptions.DockerException;
 import com.spotify.docker.client.messages.*;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
+import javax.annotation.Nonnull;
+import java.net.URI;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 
 @Mojo(name = "run",
@@ -31,10 +38,12 @@ public class RunMojo extends AbstractMojo {
     String host_ports ;
     //标签
     @Parameter(property = "dockerplugin.tag")
-    String tag;
-
+    String completetag;
+    //远程API地址
+    @Parameter(property = "dockerplugin.privaterepository")
+    String remote_HTTPSAPI;
     @Override
-    public void execute() throws MojoExecutionException{
+    public void execute() throws MojoExecutionException,MojoFailureException {
         DockerClient dc = openDockerClient();
 
         // Bind container ports to host ports
@@ -46,11 +55,16 @@ public class RunMojo extends AbstractMojo {
             portBindings.put(port, hostPorts);
         }
         final HostConfig hostConfig = HostConfig.builder().portBindings(portBindings).build();
-
+        if (!validateRepository(completetag)) {
+            throw new MojoFailureException(
+                    "标签名称\""
+                            + completetag
+                            + "\" 只能是 小写, 数字, '-', '_' , '.'和':'");
+        }
         // Create container with exposed ports
         final ContainerConfig containerConfig = ContainerConfig.builder()
                 .hostConfig(hostConfig)
-                .image(tag).exposedPorts(ports)
+                .image(completetag).exposedPorts(ports)
                 .tty(false)//后台运行
                 //.cmd("sh", "-c", "while :; do sleep 1; done") //可以自定义cmd参数
                 .build();
@@ -61,24 +75,31 @@ public class RunMojo extends AbstractMojo {
             creation = dc.createContainer(containerConfig);
             final String id = creation.id();
 
-            // 启动容器
+            //启动容器
             dc.startContainer(id);
-
-        } catch ( DockerException e ) {
-            e.printStackTrace();
-        } catch ( InterruptedException e ) {
-            e.printStackTrace();
+            System.out.println("容器启动成功！");
+        } catch ( InterruptedException|DockerException e ) {
+            throw new MojoExecutionException("容器启动失败！", e);
         }
 
         dc.close();
         return;
     }
 
+    private static final String VALID_REPO_REGEX = "^([a-z0-9_.:-])+(\\/[a-z0-9_.:-]+)*$";
 
-    private DockerClient openDockerClient() throws MojoExecutionException {
+    @VisibleForTesting
+    static boolean validateRepository(@Nonnull String repository) {
+        Pattern pattern = Pattern.compile(VALID_REPO_REGEX);
+        return pattern.matcher(repository).matches();
+    }
 
+    private DockerClient openDockerClient() throws MojoExecutionException  {
+        String caPath =System.getProperty("user.home").replaceAll("\\\\","\\\\\\\\")+".docker\\";
         try {
-            return DefaultDockerClient.fromEnv()
+
+            return DefaultDockerClient.fromEnv().uri(URI.create(remote_HTTPSAPI))
+                    .dockerCertificates(new DockerCertificates(Paths.get(caPath)))
                     .build();
         } catch ( DockerCertificateException e) {
             throw new MojoExecutionException("docker 远程 API 创建连接错误", e);
